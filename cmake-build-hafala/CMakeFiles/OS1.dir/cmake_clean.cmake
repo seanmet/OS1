72e9ba999c23d98d5c9842d61471776901589199
Guy Or <xguyor@gmail.com>
2022-12-01 19:09:48 +0200

    kimpush day 5

---
+++ cmake-build-hafala/CMakeFiles/OS1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS1.dir/Commands.cpp.o"
+  "CMakeFiles/OS1.dir/signals.cpp.o"
+  "CMakeFiles/OS1.dir/smash.cpp.o"
+  "OS1"
+  "OS1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
